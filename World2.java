efa2326573a13492be0ea29e7dcab245eb57b294
Marshal Duncan <52413587+AykoPredator@users.noreply.github.com>
2021-05-17 10:16:58 -0400

    World 2 Done

--- World2.java
+++ World2.java
@@ -26,5 +26,31 @@ public class World2 extends World
         addObject(new Wall(),100,50);
         addObject(new Wall(),100,500);
         addObject(new Wall(),100,550);
+        addObject(new Wall(),300,0);
+        addObject(new Wall(),500,600);
+        addObject(new Wall(),680,0);
+        addObject(new Barn2(),767,567);
+        addObject( new Spider(9, 0, 767), 767, 10);
+                for(int i = 0; i < 2; i++)
+        {
+            addObject( new Spider(-7, 600, 200), 200, 10 + 300*i);
+        }
+        for (int i = 0; i < 5; i++){
+            addObject(new Wall(), 300, 200 +100*i);
+        }
+                for(int i = 0; i < 3; i++)
+        {
+            addObject( new Spider(5, 0, 400), 400, 10 + 200*i);
+        }
+        for (int i = 0; i < 5; i++){
+            addObject(new Wall(), 500, 0 +100*i);
+        }
+        for (int i = 0; i < 5; i++){
+            addObject(new Wall(), 680, 200 +100*i);
+        }
+                for(int i = 0; i < 3; i++)
+        {
+            addObject( new Spider(-5, 600, 590), 590, 10 + 200*i);
+        }
     }
 }
